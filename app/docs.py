5e95802d5f4deee971e04f996716054ead75a08d
antipov <antipov@bolid.ru>
2024-03-18 16:11:57 +0300

    =-=asd

--- app/docs.py
+++ app/docs.py
@@ -8,9 +8,9 @@ app_docs: AppDocs = {
     "title": "Team-Builder",
     "description": (
         "***[Trainers"
-        " API](http://158.160.118.208/docs#/%D0%A2%D1%80%D0%B5%D0%BD%D0%B5%D1%80%D1%8B%20%E2%86%92%20%D0%A0%D0%B5%D0%B3%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D1%8F)***"
+        " API](https://tbuilder.pro/docs#/%D0%A2%D1%80%D0%B5%D0%BD%D0%B5%D1%80%D1%8B%20%E2%86%92%20%D0%A0%D0%B5%D0%B3%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D1%8F)***"
         + "\n\n ***[Sportsmans"
-        " API](http://158.160.118.208/docs#/%D0%A1%D0%BF%D0%BE%D1%80%D1%82%D1%81%D0%BC%D0%B5%D0%BD%D1%8B%20%E2%86%92%20%D0%A0%D0%B5%D0%B3%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D1%8F)***"
+        " API](https://tbuilder.pro/docs#/%D0%A1%D0%BF%D0%BE%D1%80%D1%82%D1%81%D0%BC%D0%B5%D0%BD%D1%8B%20%E2%86%92%20%D0%A0%D0%B5%D0%B3%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D1%8F)***"
         + f"""\n
     DEBUG MODE - {settings.DEBUG}
     """
