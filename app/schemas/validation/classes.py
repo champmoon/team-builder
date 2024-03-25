995adbdc34d11deca163a4658285182a5fc4e9a4
antipov <antipov@bolid.ru>
2024-03-25 16:02:08 +0300

    sad

--- app/schemas/validation/classes.py
+++ app/schemas/validation/classes.py
@@ -4,6 +4,7 @@ from pydantic import root_validator
 class BaseAtLeastOneFieldValidator:
     @root_validator(pre=True)
     def any_of(cls, v: dict) -> dict:
-        if len(v.values()) == 0 or "" not in v.values():
+        filtered_values = {k: v for k, v in v.items() if v is not None}
+        if len(filtered_values.values()) == 0:
             raise ValueError("at least one field is required")
         return v
