c6b08e1a0f8bbc48599e60e1ec827695cded8066
antipov <antipov@bolid.ru>
2024-03-26 15:28:58 +0300

    sfd

--- app/consts/workouts_statuses.py
+++ app/consts/workouts_statuses.py
@@ -5,10 +5,14 @@ class WorkoutsStatusesEnum(IntEnum):
     PLANNED = 1
     IN_PROGRESS = 2
     COMPLETED = 3
+    ACTIVE = 4
+    CANCELED = 5
 
 
 WORKOUTS_STATUSES_DESC = {
     WorkoutsStatusesEnum.PLANNED: "Запланирована",
     WorkoutsStatusesEnum.IN_PROGRESS: "В процессе",
     WorkoutsStatusesEnum.COMPLETED: "Завершена",
+    WorkoutsStatusesEnum.ACTIVE: "Активна",
+    WorkoutsStatusesEnum.CANCELED: "Отменена",
 }
