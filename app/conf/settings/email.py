5ff1dba6c3960caf2f3ab91442b16b7f1850e7d3
antipov <antipov8v@gmail.com>
2024-05-07 12:29:39 +0300

    configured mail

--- app/conf/settings/email.py
+++ app/conf/settings/email.py
@@ -8,6 +8,4 @@ class EmailSettings(BaseSettings):
     EMAIL_HOST_USER: EmailStr
     EMAIL_HOST_PASSWORD: str
     DEFAULT_FROM_EMAIL: EmailStr
-    EMAIL_USE_TLS: bool
-    EMAIL_USE_SSL: bool
     CONFIRM_URL: str
