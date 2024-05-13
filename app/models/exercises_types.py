ba53eebea987796306e7ba3e4e2bb31785d1026c
antipov <antipov@bolid.ru>
2024-05-13 13:36:25 +0300

    add migration trainer support

--- app/models/exercises_types.py
+++ app/models/exercises_types.py
@@ -1,4 +1,7 @@
-from sqlalchemy.orm import Mapped
+from uuid import UUID
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.base_class import Base
 from app.db.mixins import UUIDAsIDMixin
@@ -8,3 +11,12 @@ class ExercisesTypes(Base, UUIDAsIDMixin):
     __tablename__ = "exercises_types"
 
     type: Mapped[int]
+    description: Mapped[str]
+    is_basic: Mapped[bool]
+
+    trainer_id: Mapped[UUID] = mapped_column(
+        ForeignKey(
+            column="trainers.id",
+            ondelete="CASCADE",
+        )
+    )
